df280a960fa8491126b717a0c9c5db7ac533539c
John Mathis <johnmathis98@gmail.com>
2024-04-21 16:46:00 -0400

    Organized the Sub Card Data so that each object's data is in identical order.

--- app/components/MainCard/SubCardData.js
+++ app/components/MainCard/SubCardData.js
@@ -1,26 +1,26 @@
 export default SubCardData = {
   facebook: {
-    views: 87,
-    page_view_percentage: "3%",
     likes: 52,
-    page_likes: "2%",
-  },
-  twitter: {
-    retweets: 117,
-    retweets_percentage: 1375,
-    likes: 507,
-    likes_percentgage: 553,
+    likes_percentage: "2%",
+    supporting_statistic: 87,
+    suporting_percentage: "3%",
   },
   instagram: {
     likes: 5462,
     likes_percentage: 2257,
-    profile_views: "52k",
-    profile_view_percentage: 1375,
+    supporting_statistic: "52k",
+    supporting_percentage: 1375,
+  },
+  twitter: {
+    likes: 507,
+    likes_percentgage: 553,
+    supporting_statistic: 117,
+    suporting_percentage: 1375,
   },
   youtube: {
     likes: 107,
     likes_percentage: 19,
-    total_views: 1407,
-    total_views_percentage: 12,
+    supporting_statistic: 1407,
+    suporting_percentage: 12,
   },
 };
