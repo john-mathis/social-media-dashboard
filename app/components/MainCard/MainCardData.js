1807df0190c4104c941e40259e070e46705a2c71
John Mathis <johnmathis98@gmail.com>
2024-04-24 14:35:40 -0400

    Added image routes into the Main Card Data file.

--- app/components/MainCard/MainCardData.js
+++ app/components/MainCard/MainCardData.js
@@ -1,28 +1,28 @@
 const MainData = [
   // Facebook
   {
-    img: "",
+    img: "/images/icon-facebook.svg",
     username: "@nathanf",
     followers: "1987",
     engagement: 12,
   },
   // Twitter
   {
-    img: "",
+    img: "/images/icon-twitter.svg",
     username: "@nathanf",
     followers: "1044",
     engagement: 99,
   },
   // Instagram
   {
-    img: "",
+    img: "/images/icon-instagram.svg",
     username: "@realnathanf",
     followers: "11k",
     engagement: 1099,
   },
   // YouTube
   {
-    img: "",
+    img: "/images/icon-youtube.svg",
     username: "@realnathanf",
     followers: "8239",
     engagement: -144,
