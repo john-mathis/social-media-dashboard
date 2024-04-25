b430095bfe563b7d3440af5bd56d87a19e8d5c4d
John Mathis <johnmathis98@gmail.com>
2024-04-24 23:06:46 -0400

    Added additional styling related divs in the Main Card component.

--- app/components/MainCard/MainCard.tsx
+++ app/components/MainCard/MainCard.tsx
@@ -11,24 +11,26 @@ interface MainCardProps {
 }
 
 export default function MainCard() {
-  const mack = MainCardData.map((a) => {
+  const displayMainCard = MainCardData.map((a) => {
     return (
-      <section className="rounded-md m-auto text-center w-[20rem] h-[13rem] bg-red-500">
-        <div className="w-24 mx-auto mb-4 flex ">
-          <Image
-            src={a?.img ? a.img : ""}
-            alt="facebook icon"
-            width={20}
-            height={20}
-          />
-          <p className="text-xs">{a?.username}</p>
+      <section className="my-6 border-2 border-solid rounded-md m-auto text-center w-[20rem] h-[13rem]">
+        <div className="flex flex-col mx-auto justify-center items-center bg-red-500">
+          <div className="w-24 mx-auto mb-4 flex">
+            <Image
+              src={a?.img ? a.img : ""}
+              alt="facebook icon"
+              width={20}
+              height={20}
+            />
+            <p className="text-xs">{a?.username}</p>
+          </div>
+          <p className="text-6xl">{a?.followers}</p>
+          <p className=" text-xs uppercase tracking-[.313em]">followers</p>
+          <p className="mt-4">{a?.engagement} </p>
         </div>
-        <p className="text-6xl">{a?.followers}</p>
-        <p className=" text-xs uppercase tracking-[.313em]">followers</p>
-        <p className="mt-4">{a?.engagement} </p>
       </section>
     );
   });
 
-  return mack;
+  return displayMainCard;
 }
