004d885df09cb48141ffe536f991ac119b88ec73
John Mathis <johnmathis98@gmail.com>
2024-04-30 07:52:00 -0400

    Edited MainCard Component for reusability purposes. Intergrated the MainCardProps Interface and destructured the arguements.

--- app/components/MainCard/MainCard.tsx
+++ app/components/MainCard/MainCard.tsx
@@ -10,35 +10,37 @@ interface MainCardProps {
   engagement: any;
 }
 
-export default function MainCard() {
-  const displayMainCard = MainCardData.map((a) => {
-    return (
-      <section className="my-6 border-2 border-solid rounded-md m-auto text-center w-[20rem] h-[13rem]">
-        <div className="flex flex-col justify-center items-center mx-auto h-full">
-          <div className="w-24 mx-auto mb-4 flex">
-            <Image
-              src={a?.img ? a.img : ""}
-              alt="facebook icon"
-              width={20}
-              height={20}
-            />
-            <p className="text-xs">{a?.username}</p>
-          </div>
-          <p className="text-6xl">{a?.followers}</p>
-          <p className=" text-xs uppercase tracking-[.313em]">followers</p>
-          <p
-            className={
-              a?.engagement > 0
-                ? "text-[var(--LimeGreen)]"
-                : "text-[var(--BrightRed)]"
-            }
-          >
-            {a?.engagement}
-          </p>
+export default function MainCard({
+  img,
+  username,
+  followers,
+  engagement,
+}: MainCardProps) {
+  console.log({ MainCardData });
+  return (
+    <section className="my-6 border-2 border-solid rounded-md m-auto text-center w-[20rem] h-[13rem]">
+      <div className="flex flex-col justify-center items-center mx-auto h-full">
+        <div className="w-24 mx-auto mb-4 flex">
+          <Image
+            src={img ? img : ""}
+            alt="facebook icon"
+            width={20}
+            height={20}
+          />
+          <p className="text-xs">{username}</p>
         </div>
-      </section>
-    );
-  });
-
-  return displayMainCard;
+        <p className="text-6xl">{followers}</p>
+        <p className=" text-xs uppercase tracking-[.313em]">followers</p>
+        <p
+          className={
+            engagement > 0
+              ? "text-[var(--LimeGreen)]"
+              : "text-[var(--BrightRed)]"
+          }
+        >
+          {engagement}
+        </p>
+      </div>
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import MainCard from "./components/MainCard/MainCard";
+import MainData from "./components/MainCard/MainCardData";
 
 export default function Home() {
   return (
